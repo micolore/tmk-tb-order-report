7dc9294c4ad0686dedaef0dd18bbed519bcafcd2
kubrick <471304402@qq.com>
2022-10-12 10:51:27 +0800

    update code
    1、增加定时导出功能（指定时间的订单）

--- order-report.js
+++ order-report.js
@@ -31,12 +31,57 @@ function addButton(element, onclickFunc, value = "按钮", width = "40px", heigh
 }
 
 const orderListPage = /(http|https):\/\/buyertrade\.taobao.*?\/trade/g;
+const orderTime ="2022-10-12"
 if (orderListPage.exec(document.URL)) {
     const orderListMain = document.getElementById("J_bought_main");
-    addButton(orderListMain, addCurrentPageOrdersToList, "添加订单", "160px");
-    addButton(orderListMain, exportOrders, "导出订单", "160px");
+    addButton(orderListMain, addCurrentPageOrdersToList, "添加订单", "60px");
+    addButton(orderListMain, addCurrentPageOrdersToList, "清除订单", "60px");
+    addButton(orderListMain, exportOrders, "导出订单", "60px");
+    addButton(orderListMain, addOrdersToReportListV2, "定时导出订单", "80px");
 }
 
+// setTimeout
+let sleep = function (fun, time) {
+    setTimeout(() => {
+        fun();
+    }, time);
+}
+
+let flag = true
+// 添加订单到导出列表
+function addOrdersToReportListV2() {
+
+    // 1、循环所有的页面订单，小于指定日期直接进行返回
+    // 2、先获取当前页面的，有下一页就获取下一页，直到没有下一页。
+    get_next_page_order()
+
+    while (flag) {
+        document.getElementsByClassName("pagination-next")[0].click();
+        // 延迟执行
+        sleep(get_next_page_order, 2000);
+    }
+    console.info("添加订单成功!")
+}
+// 跳转到下一页(需要递归)
+function get_next_page_order() {
+
+    const orders = document.getElementsByClassName("js-order-container");
+    if (orders == null || orders == undefined) {
+        return
+    }
+    for (let order of orders) {
+        let items = processOrder(order);
+        if (!items) {
+            continue;
+        }
+        _.forEach(items, (value, key) => {
+            orderList[key] = value;
+        })
+    }
+    console.info(orderList)
+}
+
+// 导出csv
 function toCsv(header, data, filename) {
     let rows = "";
     let row = header.join(",");
@@ -56,6 +101,7 @@ function toCsv(header, data, filename) {
 }
 
 let orderList = {}
+// 添加当前页面的订单到导出列表
 function addCurrentPageOrdersToList() {
     const orders = document.getElementsByClassName("js-order-container");
 
@@ -94,56 +140,65 @@ function processOrder(order) {
         console.log('暂未发现订单！');
     } else {
         const date = isExist[1];
-        console.info("date:" + date)
+        console.info("订单时间:" + date)
+
         const id = order.querySelector("div[data-id]").getAttribute("data-id");
 
         let index = 0;
 
-        while (true) {
-
-            let productQuery = order.querySelector("span[data-reactid='.0.7:$order-" + id + ".$" + id + ".0.1:1:0.$" + index + ".$0.0.1.0.0.1']");
-            let priceQuery = order.querySelector("span[data-reactid='.0.7:$order-" + id + ".$" + id + ".0.1:1:0.$" + index + ".$1.0.1.1']");
-            let countQuery = order.querySelector("p[data-reactid='.0.7:$order-" + id + ".$" + id + ".0.1:1:0.$" + index + ".$2.0.0']");
-            let actualPayQuery = order.querySelector("span[data-reactid='.0.7:$order-" + id + ".$" + id + ".0.1:1:0.$" + index + ".$4.0.0.2.0.1']");
-            let itemUrlQuery = order.querySelector("a[href]");
-
-            if (productQuery === null) {
-                break;
-            }
-
-            let price = priceQuery.textContent;
-            if (productQuery.textContent == insuranceText) {
-                break
-            }
-
-            let count = countQuery.textContent;
-
-            if (actualPayQuery != null) {
-                var actualPay = actualPayQuery.textContent;
-            } else {
-                count = 1;
-            }
-
-            if (index === 0) {
-                let statusQuery = order.querySelector("span[data-reactid='.0.7:$order-" + id + ".$" + id + ".0.1:1:0.$" + index + ".$5.0.0.0']");
-                var status = statusQuery.textContent;
-            }
-
-            let itemUrl = itemUrlQuery.href
-
-            index++;
-
-            outputData[id + index] = [
-                id,
-                date,
-                productQuery.textContent.replace(/,/g, "，"),
-                itemUrl,
-                parseFloat(price),
-                count,
-                actualPay,
-                status,
-            ]
+        if (date != orderTime) {
+            return;
         }
+        if (date < orderTime) {
+            console.info("time is end!")
+            flag = false
+            return
+        }
+        let productQuery = order.querySelector("span[data-reactid='.0.7:$order-" + id + ".$" + id + ".0.1:1:0.$" + index + ".$0.0.1.0.0.1']");
+        let priceQuery = order.querySelector("span[data-reactid='.0.7:$order-" + id + ".$" + id + ".0.1:1:0.$" + index + ".$1.0.1.1']");
+        let countQuery = order.querySelector("p[data-reactid='.0.7:$order-" + id + ".$" + id + ".0.1:1:0.$" + index + ".$2.0.0']");
+        let actualPayQuery = order.querySelector("span[data-reactid='.0.7:$order-" + id + ".$" + id + ".0.1:1:0.$" + index + ".$4.0.0.2.0.1']");
+        let itemUrlQuery = order.querySelector("a[href]");
+
+        if (productQuery === null) {
+            return;
+        }
+
+        let price = priceQuery.textContent;
+        if (productQuery.textContent == insuranceText) {
+            return;
+        }
+
+        let count = countQuery.textContent;
+        let actualPay;
+        if (actualPayQuery != null) {
+            // 实付金额
+            actualPay = actualPayQuery.textContent;
+        } else {
+            count = 1;
+        }
+
+        if (index === 0) {
+            let statusQuery = order.querySelector("span[data-reactid='.0.7:$order-" + id + ".$" + id + ".0.1:1:0.$" + index + ".$5.0.0.0']");
+            // 订单状态
+            let orderStatus = statusQuery.textContent;
+            console.info(orderStatus)
+        }
+
+        let itemUrl = itemUrlQuery.href
+
+        index++;
+
+        outputData[id + index] = [
+            id,
+            date,
+            productQuery.textContent.replace(/,/g, "，"),
+            itemUrl,
+            parseFloat(price),
+            count,
+            actualPay,
+            status,
+        ]
     }
     return outputData;
 }
